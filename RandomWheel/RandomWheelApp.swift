66a829b8ec04b42cad208c8d2c61b8fdd6e04907
WoodenDoor <vincent031525@gmail.com>
2025-04-20 16:40:45 +0800

    Initial Commit

---
+++ RandomWheel/RandomWheelApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomWheelApp.swift
+//  RandomWheel
+//
+//  Created by 木門 on 2025/4/20.
+//
+
+import SwiftUI
+
+@main
+struct RandomWheelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
